df042ce340d78c4e689bda3080141452fd67ecf3
bear <xwpcom@163.com>
2021-09-07 16:31:45 +0800

    s

--- include/core/net/urltool.h
+++ include/core/net/urltool.h
@@ -8,6 +8,9 @@ class CORE_EXPORT UrlTool
 public:
 	static std::string GetUriTitle(std::string url);
 	static string PercentHexString(const string& text);
+	static string UrlEncode(const string& str);
+	static char CharToInt(char ch);
+	static char StrToBin(const char* str);
 };
 
 }
--- src/core/net/urltool.cpp
+++ src/core/net/urltool.cpp
@@ -84,6 +84,43 @@ string UrlTool::GetUriTitle(string url)
 }
 #endif
 
+char UrlTool::CharToInt(char ch)
+{
+	if (ch >= '0' && ch <= '9')return (char)(ch - '0');
+	if (ch >= 'a' && ch <= 'f')return (char)(ch - 'a' + 10);
+	if (ch >= 'A' && ch <= 'F')return (char)(ch - 'A' + 10);
+	return -1;
+}
+
+char UrlTool::StrToBin(const char* str)
+{
+	char tempWord[2];
+	char chn;
+	tempWord[0] = CharToInt(str[0]); //make the B to 11 -- 00001011 
+	tempWord[1] = CharToInt(str[1]); //make the 0 to 0 -- 00000000 
+	chn = (tempWord[0] << 4) | tempWord[1]; //to change the BO to 10110000 
+	return chn;
+}
+
+// from zlmediakit
+string UrlTool::UrlEncode(const string& str)
+{
+	string out;
+	size_t len = str.size();
+	for (size_t i = 0; i < len; ++i) {
+		char ch = str[i];
+		if (isalnum((uint8_t)ch) || ch == '-') {
+			out.push_back(ch);
+		}
+		else {
+			char buf[4];
+			sprintf(buf, "%%%X%X", (uint8_t)ch >> 4, (uint8_t)ch & 0x0F);
+			out.append(buf);
+		}
+	}
+	return out;
+}
+
 }
 }
 }
