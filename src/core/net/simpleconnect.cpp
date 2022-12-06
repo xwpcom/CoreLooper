7a1c03227339f93976928a84de4696637c3c0a8e
bear <xwpcom@163.com>
2022-12-06 18:43:45 +0800

    s

--- src/core/net/simpleconnect.cpp
+++ src/core/net/simpleconnect.cpp
@@ -219,7 +219,7 @@ void SimpleConnect::OnTimer(long id)
 {
 	if (id == mTimer_AutoClose)
 	{
-		LogV(TAG,"%s timeout auto close,this=%p,mTimeOutSecond=%d", GetObjectName().c_str(),this, mTimeOutSecond);
+		//LogV(TAG,"%s timeout auto close,this=%p,mTimeOutSecond=%d", GetObjectName().c_str(),this, mTimeOutSecond);
 		Destroy();
 		return;
 	}
