9710b6dc535e011e0b0f160ea14f27b3e4c72c7d
bear <xwpcom@163.com>
2023-10-27 12:04:30 +0800

    s

--- demo/ftpserver/src/mainlooper.cpp
+++ demo/ftpserver/src/mainlooper.cpp
@@ -7,6 +7,39 @@ using namespace Bear::Core;
 using namespace Bear::Core::Net::Ftp;
 using namespace Bear::Core::Net::Http;
 
+class Ajax_Test :public AjaxHandler
+{
+	SUPER(AjaxHandler);
+	DECLARE_AJAX_CLASS(Ajax_Test);
+	string mTag = "test";
+public:
+
+	string Process(const NameValue& params)
+	{
+		int ret = 0;
+		DynamicJsonBuffer jBuffer;
+		auto& json = jBuffer.createObject();
+
+		{
+			auto uid = params.GetString("uid");
+			auto tag = params.GetString("tag");
+			LogV(mTag, "");
+
+			json["error"] = ret;
+			if (ret == 0)
+			{
+				json["uid"] = uid;
+				json["tag"] = tag;
+			}
+		}
+
+		string ack;
+		json.printTo(ack);
+		return ack;
+	}
+};
+IMPLEMENT_AJAX_CLASS(Ajax_Test, "test.json", "")
+
 MainLooper::MainLooper()
 {
 }
@@ -32,11 +65,22 @@ void MainLooper::OnCreate()
 	}
 
 
+
 	{
 		//for demo
 
 		auto config = make_shared<tagWebServerConfig>();
 		config->mWebRootFolder = "d:/test/webRoot/";
+
+		auto ajaxHandler = make_shared<AjaxCommandHandler>();
+		AddChild(ajaxHandler);
+		config->mAjaxCommandHandler = ajaxHandler;
+		{
+			auto jsonHandler = make_shared<JsonHandler>();
+			AddChild(jsonHandler);
+			config->mJsonHandler = jsonHandler;
+		}
+
 		auto svr(make_shared<HttpServer>());
 		svr->SetConfig(config);
 
